9877b5674b88acc5d38ff55511a284b78732a33a
DJ <konobeat@gmail.com>
2018-07-30 18:51:48 -0400

    ??

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+var port = 8080;
+
+app.get("/", function (req, res) {
+    res.send("Welcome!");
+});
+
+app.listen(port, function () {
+     console.log("App listening on PORT " + port);
+});
